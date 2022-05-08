18d64f5c07e3b74c12f6b04521daf90ed4420afe
Naiane Negri <naiannegri@gmail.com>
2022-05-08 15:15:36 -0300

    exercicio semana sete

--- Para Casa/AtividadeParaCasa.js
+++ Para Casa/AtividadeParaCasa.js
@@ -37,14 +37,12 @@ function enviarEmail(corpo, para){
 
 // aqui vc pode chamar uma promise dentro de outra(Promisses aninhadas ou Promisse Hell)
 console.log("Inicio!");
-pegarId().then((id) => {
-    buscarEmailNoBanco(id).then((email) => { 
-        enviarEmail("Olá, como vai?",email).then(() => {
-            console.log("Email enviado, para o usuário com id: " + id)
-        }).catch(err => {
-            console.log(err);
-        })
-        
-    })
-})
-console.log("Foi!");
+
+async function principal(){
+    let pegaIds = pegarId()
+    let buscarEmail = await buscarEmailNoBanco()
+    let enviaEmail =  await enviarEmail()
+    console.log("Foi!");
+}
+principal()
+
