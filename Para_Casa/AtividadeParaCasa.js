5b8d45ccae442c1320c0b62c87496528805d8da0
Carol Alves <prof.carolalves90@gmail.com>
2022-05-09 19:48:50 -0300

    :ok_hand: Alterações de revisão de código

---
+++ Para_Casa/AtividadeParaCasa.js
@@ -0,0 +1,50 @@
+
+/* Desafio para casa:  Transformar a nossa promisse Hell em async_await
+Dica de ouro: Vai ter uma das promises que vc não vai precisar usar await
+*/
+
+
+
+function pegarId(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(5)   
+        },1500)
+    })
+}
+
+function buscarEmailNoBanco(id){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("jenifer@reprograma.com")
+        },2000);
+    })
+}
+
+function enviarEmail(corpo, para){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {  
+            var deuErro = false;
+            if(!deuErro){
+                resolve({time: 6, to: "jenifer@reprograma.com"}) // Promessa OK!
+            }else{
+                reject("Fila cheia") // Foi mal, eu falhei :(
+            }
+        },4000)
+    });
+}
+
+
+// aqui vc pode chamar uma promise dentro de outra(Promisses aninhadas ou Promisse Hell)
+console.log("Inicio!");
+pegarId().then((id) => {
+    buscarEmailNoBanco(id).then((email) => { 
+        enviarEmail("Olá, como vai?",email).then(() => {
+            console.log("Email enviado, para o usuário com id: " + id)
+        }).catch(err => {
+            console.log(err);
+        })
+        
+    })
+})
+console.log("Foi!");
---
+++ Para_Casa/tarefaRealizada.js
@@ -0,0 +1,38 @@
+function pegarId(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(5)   
+        },1500)
+    })
+}
+
+function buscarEmailNoBanco(id){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("jenifer@reprograma.com")
+        },2000);
+    })
+}
+
+function enviarEmail(corpo, para){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {  
+            var deuErro = false;
+            if(!deuErro){
+                resolve({time: 6, to: "jenifer@reprograma.com"}) // Promessa OK!
+            }else{
+                reject("Fila cheia") // Foi mal, eu falhei :(
+            }
+        },4000)
+    });
+}
+
+async function organizaEnvio(){
+    const id = pegarId()
+    const buscaEmail = await buscarEmailNoBanco(id)
+    const envia = await enviarEmail(buscaEmail)
+    console.log(envia)
+}
+
+organizaEnvio()
+
