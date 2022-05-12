7a157d4e3e4bc429e0d51ef69cf7072ec4b9fc29
Victoria Scholte <victoria.scholte@gmail.com>
2022-05-11 21:28:48 -0300

    add async_await

--- Para Casa/AtividadeParaCasa.js
+++ Para Casa/AtividadeParaCasa.js
@@ -35,16 +35,27 @@ function enviarEmail(corpo, para){
 }
 
 
-// aqui vc pode chamar uma promise dentro de outra(Promisses aninhadas ou Promisse Hell)
-console.log("Inicio!");
-pegarId().then((id) => {
-    buscarEmailNoBanco(id).then((email) => { 
-        enviarEmail("Olá, como vai?",email).then(() => {
-            console.log("Email enviado, para o usuário com id: " + id)
-        }).catch(err => {
-            console.log(err);
-        })
+// // aqui vc pode chamar uma promise dentro de outra(Promisses aninhadas ou Promisse Hell)
+// console.log("Inicio!");
+// pegarId().then((id) => {
+//     buscarEmailNoBanco(id).then((email) => { 
+//         enviarEmail("Olá, como vai?",email).then(() => {
+//             console.log("Email enviado, para o usuário com id: " + id)
+//         }).catch(err => {
+//             console.log(err);
+//         })
         
-    })
+//     })
+// })
+// console.log("Foi!");
+
+async function principal(){
+    let id = await pegarId()
+    let email = await buscarEmailNoBanco(id)
+    enviarEmail('Olá, esse é um email', email).then(() => {
+        console.log('Email enviado')
+}).catch((err) => {
+    console.log((err))
 })
-console.log("Foi!");
+}
+principal()
