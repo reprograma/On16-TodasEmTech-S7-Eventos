dd628ece452da6cd754600c543c5b622bac43c5c
Ana Ribeiro <analidialirar@gmail.com>
2022-05-13 15:17:04 -0300

    feature: add resolution for homework

--- ParaCasa/AtividadeParaCasa.js
+++ ParaCasa/AtividadeParaCasa.js
@@ -3,9 +3,7 @@
 Dica de ouro: Vai ter uma das promises que vc não vai precisar usar await
 */
 
-
-
-function pegarId(){
+function getId(){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(5)   
@@ -13,20 +11,20 @@ function pegarId(){
     })
 }
 
-function buscarEmailNoBanco(id){
+function searchEmailDataBase(id){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve("jenifer@reprograma.com")
+            resolve("analidialirar@gmail.com")
         },2000);
     })
 }
 
-function enviarEmail(corpo, para){
+function sendEmail(corpo, para){
     return new Promise((resolve, reject) => {
         setTimeout(() => {  
             var deuErro = false;
             if(!deuErro){
-                resolve({time: 6, to: "jenifer@reprograma.com"}) // Promessa OK!
+                resolve({time: 6, to: "analidialirar@gmail.com"}) // Promessa OK!
             }else{
                 reject("Fila cheia") // Foi mal, eu falhei :(
             }
@@ -34,12 +32,11 @@ function enviarEmail(corpo, para){
     });
 }
 
-
 // aqui vc pode chamar uma promise dentro de outra(Promisses aninhadas ou Promisse Hell)
 console.log("Inicio!");
-pegarId().then((id) => {
-    buscarEmailNoBanco(id).then((email) => { 
-        enviarEmail("Olá, como vai?",email).then(() => {
+getId().then((id) => {
+    searchEmailDataBase(id).then((email) => { 
+        sendEmail("Olá, como vai?",email).then(() => {
             console.log("Email enviado, para o usuário com id: " + id)
         }).catch(err => {
             console.log(err);
@@ -48,3 +45,18 @@ pegarId().then((id) => {
     })
 })
 console.log("Foi!");
+
+// RESOLUÇÃO DA ATIVIDADE DE CASA
+
+async function organizeSendings(){
+    const id = await getId()
+    const email = await searchEmailDataBase(id)
+    sendEmail("Olá, como vai?", email).then(() => {
+        console.log('Email enviado, para o usuário com id: ' + id)
+        
+    }).catch((err) => {
+        console.log((err))
+    })
+}
+
+organizeSendings()
