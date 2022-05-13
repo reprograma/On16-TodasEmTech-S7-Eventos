e3e385af77501d68b8507b9f6a8993d5d9bb33ee
Sara <saragiannini593@gmail.com>
2022-05-13 16:07:19 -0300

    Atividades solucionadas

--- Aula/Sync_Assync/sincrono.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-
-console.log("Inicio")
-
-fs.writeFileSync('arquivo.txt', 'Olá Reprograma')
-console.log('Fim')
--- ParaCasa/AtividadeParaCasa.js
+++ ParaCasa/AtividadeParaCasa.js
@@ -5,46 +5,57 @@ Dica de ouro: Vai ter uma das promises que vc não vai precisar usar await
 
 
 
-function pegarId(){
+function pegarId() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(5)   
-        },1500)
+        }, 2000)
     })
 }
 
-function buscarEmailNoBanco(id){
+function buscarEmailNoBanco(id) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve("jenifer@reprograma.com")
-        },2000);
+            resolve('saragiannini593@gmail.com')
+        }, 2000);
     })
 }
 
-function enviarEmail(corpo, para){
+function enviarEmail(corpo, para) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {  
             var deuErro = false;
-            if(!deuErro){
-                resolve({time: 6, to: "jenifer@reprograma.com"}) // Promessa OK!
-            }else{
-                reject("Fila cheia") // Foi mal, eu falhei :(
+            if(!deuErro) {
+                resolve({time: 4, to: 'saragiannini593@gail.com'})
+            } else {
+                reject('Fila cheia')
             }
-        },4000)
+        }, 2000)
     });
 }
 
+async function email() {
+    const id = await pegarId()
+    const email = await buscarEmailNoBanco(id)
+    enviarEmail('Olá, como você está?', email).then(() => {
+        console.log('Seu email foi enviado com sucesso.')
+    }).catch((err) => {
+        console.log('Falha ao enviar')
+    })
+}
+
+email()
 
 // aqui vc pode chamar uma promise dentro de outra(Promisses aninhadas ou Promisse Hell)
-console.log("Inicio!");
-pegarId().then((id) => {
-    buscarEmailNoBanco(id).then((email) => { 
-        enviarEmail("Olá, como vai?",email).then(() => {
-            console.log("Email enviado, para o usuário com id: " + id)
-        }).catch(err => {
-            console.log(err);
-        })
+// console.log("Inicio!");
+// pegarId().then((id) => {
+//     buscarEmailNoBanco(id).then((email) => { 
+//         enviarEmail("Olá, como vai?",email).then(() => {
+//             console.log("Email enviado, para o usuário com id: " + id)
+//         }).catch(err => {
+//             console.log(err);
+//         })
         
-    })
-})
-console.log("Foi!");
+//     })
+// })
+// console.log("Foi!");
