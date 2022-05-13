fdc9a9ba34118e618e3f062bdab5e9a9471e50fb
Ludmila Maia <ludmilacmaia@gmail.com>
2022-05-13 17:09:21 -0300

    Resolução da Atividade para casa

--- Para Casa/AtividadeParaCasa.js
+++ Para Casa/AtividadeParaCasa.js
@@ -35,16 +35,30 @@ function enviarEmail(corpo, para){
 }
 
 
-// aqui vc pode chamar uma promise dentro de outra(Promisses aninhadas ou Promisse Hell)
-console.log("Inicio!");
-pegarId().then((id) => {
-    buscarEmailNoBanco(id).then((email) => { 
-        enviarEmail("Olá, como vai?",email).then(() => {
-            console.log("Email enviado, para o usuário com id: " + id)
-        }).catch(err => {
-            console.log(err);
-        })
+// // aqui vc pode chamar uma promise dentro de outra(Promisses aninhadas ou Promisse Hell)
+// console.log("Inicio!");
+// pegarId().then((id) => {
+//     buscarEmailNoBanco(id).then((email) => { 
+//         enviarEmail("Olá, como vai?",email).then(() => {
+//             console.log("Email enviado, para o usuário com id: " + id)
+//         }).catch(err => {
+//             console.log(err);
+//         })
         
-    })
-})
-console.log("Foi!");
+//     })
+// })
+// console.log("Foi!");
+
+async function email(){
+    try{
+        const id = await pegarId()
+        const emailUsuario = await buscarEmailNoBanco(id)
+        const enviarParaUsuario = enviarEmail("Olá, usuário!", emailUsuario)
+	console.log("email enviado com sucesso")
+    } catch(error){
+            console.log(error)
+    }
+
+
+}
+email()
