ed6777d2fd2f910f8595959bbb3b289cc03a9bb7
caroliinaasilva <carolinadesousa84@yahoo.com>
2022-05-13 10:24:55 -0300

    Entrega de Atividade

--- ParaCasa/AtividadeParaCasa.js
+++ ParaCasa/AtividadeParaCasa.js
@@ -34,17 +34,15 @@ function enviarEmail(corpo, para){
     });
 }
 
+async function principal (){
+    let id = await pegarId()
+    let email = await  buscarEmailNoBanco(id)
+    enviarEmail('Oi linda', email).then(() => {
+       console.log('Email Enviado ihull')
 
-// aqui vc pode chamar uma promise dentro de outra(Promisses aninhadas ou Promisse Hell)
-console.log("Inicio!");
-pegarId().then((id) => {
-    buscarEmailNoBanco(id).then((email) => { 
-        enviarEmail("Olá, como vai?",email).then(() => {
-            console.log("Email enviado, para o usuário com id: " + id)
-        }).catch(err => {
-            console.log(err);
-        })
-        
+}).catch((err) => {
+        console.log((err))
     })
-})
-console.log("Foi!");
+}
+
+principal ()
