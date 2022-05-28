3d8b12f8ad5ef5c94130c3a122865e76462c5f00
Mariane Monteiro <marimonteironasciment@gmail.com>
2022-05-27 21:38:13 -0300

    resolução do exercício para casa

--- ParaCasa/AtividadeParaCasa.js
+++ ParaCasa/AtividadeParaCasa.js
@@ -3,8 +3,6 @@
 Dica de ouro: Vai ter uma das promises que vc não vai precisar usar await
 */
 
-
-
 function pegarId(){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -26,25 +24,23 @@ function enviarEmail(corpo, para){
         setTimeout(() => {  
             var deuErro = false;
             if(!deuErro){
-                resolve({time: 6, to: "jenifer@reprograma.com"}) // Promessa OK!
+                resolve({time: 6, to: "jenifer@reprograma.com"}) 
             }else{
-                reject("Fila cheia") // Foi mal, eu falhei :(
+                reject("Fila cheia") 
             }
         },4000)
     });
 }
 
+async function envioEmail(){
+    const id = await pegarId()
+    const email = await buscarEmailNoBanco(id)
 
-// aqui vc pode chamar uma promise dentro de outra(Promisses aninhadas ou Promisse Hell)
-console.log("Inicio!");
-pegarId().then((id) => {
-    buscarEmailNoBanco(id).then((email) => { 
-        enviarEmail("Olá, como vai?",email).then(() => {
-            console.log("Email enviado, para o usuário com id: " + id)
-        }).catch(err => {
-            console.log(err);
-        })
-        
+    enviarEmail("Olá, como vai?",email).then(() => {
+        console.log("Email enviado, para o usuário com id: " + id + " " + email)
+    }).catch(err => {
+        console.log(err);
     })
-})
-console.log("Foi!");
+}
+
+envioEmail()
